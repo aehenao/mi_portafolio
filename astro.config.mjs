049d159ff2dfb383c74bc8bcb3bf5dafa69d4533
“Andres <andrestivenhenao@gmail.com>
2026-03-31 15:22:56 -0500

    Agrego configuración para publicarlo en Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aehenao.com', // Replace with your domain
+  site: 'https://aehenao.github.io', // Cambia 'aehenao' por tu usuario de GitHub
+  base: '/', // Cambia esto si el sitio no está en la raíz (ej: '/landing/')
   integrations: [
     react(),
     sitemap(),
