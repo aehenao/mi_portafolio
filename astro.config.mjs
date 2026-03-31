068b3854634e44ebe60a1bdc88ec78c0d9c1f002
“Andres <andrestivenhenao@gmail.com>
2026-03-31 15:30:31 -0500

    Ajuste

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aehenao.github.io', // Cambia 'aehenao' por tu usuario de GitHub
-  base: '/', // Cambia esto si el sitio no está en la raíz (ej: '/landing/')
+  base: 'mi_portafolio',
   integrations: [
     react(),
     sitemap(),
